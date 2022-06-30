42076002bad676c1bd96377dd3f34916c19e1db3
CarlosHenriquePinheiroCordeiro <carloshenpincord@gmail.com>
2022-06-29 21:35:15 -0300

    Implementação de escritura de arquivo txt, com o objetivo de brevemete guardar o código gerado pelo compilador

--- src/objetos/GeraCodigo.java
+++ src/objetos/GeraCodigo.java
@@ -1,5 +1,10 @@
 package objetos;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class GeraCodigo {
 
 	private Programa programa;
@@ -9,10 +14,24 @@ public class GeraCodigo {
 	}
 	
 	public void gerarCodigo() {
-		getPrograma().geraCodigo();
+		escreverArquivo("codigo-gerado.txt", "Hello World");
+		//getPrograma().geraCodigo();
 		//OBS: O program(...) {...} NÃO PRECISA GERAR CÓDIGO, A ÚNICA COISA
 		//QUE PRECISA GERAR DELE É INSTANCIAR AS VARIÁVEIS PASSADAS 
 		//COMO PARÂMETRO A PARTIR DO PROGRAM, E TAMBÉM O NOME DO program COMO LABEL
+	}
+	
+	public void escreverArquivo(String caminho, String conteudo) {
+		try (
+				FileWriter     criadorArquivo 	= new FileWriter(caminho, false);
+				BufferedWriter buffer 			= new BufferedWriter(criadorArquivo);
+				PrintWriter    escritorArquivo 	= new PrintWriter(buffer);
+			)
+		{
+			escritorArquivo.append(conteudo);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public Programa getPrograma() {
